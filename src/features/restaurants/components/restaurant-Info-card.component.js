84a56f1a033e65e339f79e9dac64f5502da9ea15
sml99 <smailovdahmani@gmail.com>
2021-09-17 22:38:28 +0100

    Theme

--- src/features/restaurants/components/restaurant-Info-card.component.js
+++ src/features/restaurants/components/restaurant-Info-card.component.js
@@ -1,31 +1,33 @@
 import React from "react";
-import { Card } from "react-native-paper";
 import styled from "styled-components/native";
-
-const Title = styled.Text`
-    padding: 16px;
-`;
+import { Text } from "react-native";
+import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
-    background-color: #fff;
+    background-color: white;
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-    background-color: #fff;
-    padding: 12px;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled(Text)`
+    padding: 16px;
+    color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
-        name = "E1even",
+        name = "Some Restaurant",
         icon,
         photos = [
-            "https://images.pexels.com/photos/6443989/pexels-photo-6443989.jpeg",
+            "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
         ],
         address = "100 some random street",
         isOpenNow = true,
-        rating = 4.3,
-        isClosedTemporarily = false,
+        rating = 4,
+        isClosedTemporarily,
     } = restaurant;
 
     return (
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { StatusBar, Platform } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 import styled from "styled-components/native";
 
-import { RestaurantInfoCard } from "../components/restaurant-Info-card.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
-const isAndroid = Platform.OS === "android";
-
-const SafeArea = styled.SafeAreaView`
+const SafeArea = styled(SafeAreaView)`
     flex: 1;
-    margin-top: ${isAndroid ? StatusBar.currentHeight : 0}px;
+    margin-top: ${StatusBar.currentHeight ?? 0}px;
 `;
 
 const SearchContainer = styled.View`
@@ -19,25 +17,16 @@ const SearchContainer = styled.View`
 const RestaurantListContainer = styled.View`
     flex: 1;
     padding: 16px;
-    background-color: #eee;
+    background-color: blue;
 `;
 
-export const RestaurantsScreen = () => {
-    const [searchQuery, setSearchQuery] = React.useState("");
-    const onChangeSearch = (query) => setSearchQuery(query);
-
-    return (
-        <SafeArea>
-            <SearchContainer>
-                <Searchbar
-                    placeholder="Search"
-                    onChangeText={onChangeSearch}
-                    value={searchQuery}
-                />
-            </SearchContainer>
-            <RestaurantListContainer>
-                <RestaurantInfoCard />
-            </RestaurantListContainer>
-        </SafeArea>
-    );
-};
+export const RestaurantsScreen = () => (
+    <SafeArea>
+        <SearchContainer>
+            <Searchbar />
+        </SearchContainer>
+        <RestaurantListContainer>
+            <RestaurantInfoCard />
+        </RestaurantListContainer>
+    </SafeArea>
+);
