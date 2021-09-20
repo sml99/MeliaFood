bf1796b45bc2b2b2d98f5af399760c47687b0241
sml99 <smailovdahmani@gmail.com>
2021-09-20 12:50:48 +0100

    Navigation

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ThemeProvider } from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
 import {
     useFonts as useOswald,
     Oswald_400Regular,
@@ -9,6 +12,8 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { Text } from "./src/components/typography/text.component";
 
 export default function App() {
     const [oswaldLoaded] = useOswald({
@@ -22,10 +27,88 @@ export default function App() {
         return null;
     }
 
+    function HomeScreen() {
+        return <RestaurantsScreen />;
+    }
+
+    const SettingScreen = styled.View`
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+    `;
+
+    function SettingsScreen() {
+        return (
+            <SettingScreen>
+                <Text>Settings!</Text>
+            </SettingScreen>
+        );
+    }
+
+    function MapScreen() {
+        return (
+            <SettingScreen>
+                <Text>Settings!</Text>
+            </SettingScreen>
+        );
+    }
+
+    const Tab = createBottomTabNavigator();
     return (
         <>
             <ThemeProvider theme={theme}>
-                <RestaurantsScreen />
+                <NavigationContainer>
+                    <Tab.Navigator
+                        screenOptions={({ route }) => ({
+                            tabBarIcon: ({ focused, color, size }) => {
+                                if (route.name === "Restaurants") {
+                                    return (
+                                        <Ionicons
+                                            name={
+                                                focused
+                                                    ? "restaurant-outline"
+                                                    : "restaurant"
+                                            }
+                                            size={size}
+                                            color={color}
+                                        />
+                                    );
+                                } else if (route.name === "Settings") {
+                                    return (
+                                        <Ionicons
+                                            name={
+                                                focused
+                                                    ? "settings-outline"
+                                                    : "settings"
+                                            }
+                                            size={size}
+                                            color={color}
+                                        />
+                                    );
+                                } else if (route.name === "Map") {
+                                    return (
+                                        <Ionicons
+                                            name={
+                                                focused ? "map-outline" : "map"
+                                            }
+                                            size={size}
+                                            color={color}
+                                        />
+                                    );
+                                }
+                            },
+                            tabBarInactiveTintColor: "gray",
+                            tabBarActiveTintColor: "tomato",
+                        })}
+                    >
+                        <Tab.Screen name="Restaurants" component={HomeScreen} />
+                        <Tab.Screen name="Map" component={MapScreen} />
+                        <Tab.Screen
+                            name="Settings"
+                            component={SettingsScreen}
+                        />
+                    </Tab.Navigator>
+                </NavigationContainer>
             </ThemeProvider>
             <ExpoStatusBar style="auto" />
         </>
