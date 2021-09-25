a3dbbfe2b9c66bdc3b7a9650762abf66bcf894df
sml99 <smailovdahmani@gmail.com>
2021-09-25 12:07:16 +0100

    Account Screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import {
+    AccountBackground,
+    AccountContainer,
+    AccountCover,
+    AuthButton,
+} from "../components/account.styles";
 
-export const AccountScreen = () => {
-    return <></>;
+export const AccountScreen = ({ navigation }) => {
+    return (
+        <AccountBackground>
+            <AccountCover />
+            <AccountContainer>
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={() => navigation.navigate("Login")}
+                >
+                    Login
+                </AuthButton>
+                <Spacer direction="bottom" size="large" />
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={() => navigation.navigate("Register")}
+                >
+                    Register
+                </AuthButton>
+            </AccountContainer>
+        </AccountBackground>
+    );
 };
